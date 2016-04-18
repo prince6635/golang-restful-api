49c12f60602e08544abb3b60b489a63690f6bf87
Zi Wang <ziwang@uber.com>
2016-04-18 14:31:32 -0700

    Add logging

--- src/service/routes.go
+++ src/service/routes.go
@@ -39,11 +39,17 @@ var routes = Routes{
 func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
+		// utilize the logger decorator in our routes
+		var handler http.Handler
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
 		router.
 			Methods(route.Method).
 			Path(route.Pattern).
 			Name(route.Name).
-			Handler(route.HandlerFunc)
+			// Handler(route.HandlerFunc)
+			Handler(handler)
 	}
 
 	return router
